10bf74f9fe765a6e8f0ae612d795bc90bc1b9162
GABRIELCARDOSO11 <gabrieloliver56@gmail.com>
2025-04-15 23:17:23 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const App = () => <h1>Gerenciador de Pedidos</h1>
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
